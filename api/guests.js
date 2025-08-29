78e0067b4c77ce7dfa1ea4793fd1ca07c5d78f51
thuyanduong <thuyanannduong@gmail.com>
2025-08-29 19:54:21 -0400

    cleaned up stats

--- api/guests.js
+++ api/guests.js
@@ -142,7 +142,7 @@ export default async function handler(req, res) {
             }
         ]).toArray();
 
-        res.status(200).json({ stats });
+        res.status(200).json(stats[0]);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Failed to fetch data" });
