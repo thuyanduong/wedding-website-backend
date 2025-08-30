1cf4feae32aa6d677b62288f9e4ff2f0d4a25e65
thuyanduong <thuyanannduong@gmail.com>
2025-08-30 04:14:21 -0400

    submit rsvp

--- api/responses.js
+++ api/responses.js
@@ -1,6 +1,6 @@
 // /api/responses/
 
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import 'dotenv/config';
 
 let cachedClient = null;
@@ -25,12 +25,44 @@ export default async function handler(req, res) {
     }
 
     const db = cachedClient.db("rsvp");
-    const collection = db.collection("responses"); // your collection name
+    const collection = db.collection("responses");
 
-    // Simple find query
-    const responses = await collection.find({}).toArray();
+    if (req.method === "GET") {
+      const responses = await collection.find({}).toArray();
+      return res.status(200).json({ responses });
+    }
 
-    res.status(200).json({ responses });
+    if (req.method === "POST") {
+      const newResponse = req.body;
+      console.log(newResponse)
+      const filter = { _id: new ObjectId(newResponse.id) }
+      const mutation = {}
+      mutation.updated_at = new Date()
+      mutation.email = newResponse.email
+      mutation.phone = newResponse.phone
+      mutation.comments = newResponse.comments
+      mutation.guests = []
+
+      for(const guest of newResponse.guests){
+        if (guest.attend_wedding === false) {
+          guest.wedding_entree = null
+        }
+        mutation.guests.push(guest)
+      }
+      console.log(mutation)
+      const result = await collection.updateOne(filter, { $set: mutation });
+      if (result.modifiedCount === 1) {
+        return res.status(201).json({ message: "Response recorded", result });
+
+      } else {
+        return res.status(400).json({ message: "Error: Response failed to save", result });
+
+      }
+    }
+
+    // Method not allowed
+    res.setHeader("Allow", "GET,POST,OPTIONS");
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch data" });
